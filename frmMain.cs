e48fcbcafd54651e074b697148e1ce453e07fe23
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-11-14 12:29:38 +0100

    Clean up unused codes.

--- frmMain.cs
+++ frmMain.cs
@@ -186,62 +186,6 @@ namespace Word_To_PDF
             mainPart.Document.Body.Append(imageParagraph);
         }
 
-        public static void InsertImageOld(MainDocumentPart mainPart, string imagePath)
-        {
-            ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
-            using (FileStream stream = new FileStream(imagePath, FileMode.Open))
-            {
-                imagePart.FeedData(stream);
-            }
-
-            string imageRelationshipId = mainPart.GetIdOfPart(imagePart);
-
-            // Define the image dimensions
-            long cx = 3000000L; // Width in EMUs
-            long cy = 2000000L; // Height in EMUs
-
-            // Create a drawing element with the image
-            Drawing drawing = new Drawing();
-            DW.Inline inline = new DW.Inline(
-                new DW.Extent() { Cx = cx, Cy = cy },
-                new DW.EffectExtent() { LeftEdge = 0L, TopEdge = 0L, RightEdge = 0L, BottomEdge = 0L },
-                new DW.DocProperties() { Id = (UInt32Value)1U, Name = "Picture 1" },
-                new DW.NonVisualGraphicFrameDrawingProperties(
-                    new A.GraphicFrameLocks() { NoChangeAspect = true }),
-                new A.Graphic(
-                    new A.GraphicData(
-                        new PIC.Picture(
-                            new PIC.NonVisualPictureProperties(
-                                new PIC.NonVisualDrawingProperties() { Id = (UInt32Value)0U, Name = "New Bitmap Image.jpg" },
-                                new PIC.NonVisualPictureDrawingProperties()),
-                            new PIC.BlipFill(
-                                new A.Blip(
-                                    new A.BlipExtensionList(
-                                        new A.BlipExtension() { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" }),
-                                    new A.BlipExtension() { Uri = "{05A4C25C-085E-4A06-A0C0-9D07862D23EE}" })
-                                {
-                                    Embed = imageRelationshipId,
-                                    CompressionState = A.BlipCompressionValues.Print,
-                                },
-                                new A.Stretch(
-                                    new A.FillRectangle())),
-                            new PIC.ShapeProperties(
-                                new A.Transform2D(
-                                    new A.Offset() { X = 0L, Y = 0L },
-                                    new A.Extents() { Cx = cx, Cy = cy }),
-                                new A.PresetGeometry(
-                                    new A.AdjustValueList())
-                                { Preset = A.ShapeTypeValues.Rectangle }))
-                    //{ Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
-                    //{ Macro = "" }));
-            )));
-            inline.Append(drawing);
-
-            // Add the image to the paragraph
-            A.Paragraph imageParagraph = new A.Paragraph(new A.Run(inline));
-            mainPart.Document.Body.Append(imageParagraph);
-        }
-
         private void Panel1_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
