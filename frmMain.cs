eb2e69a66eea94e75d4fa607213daaa3f99afec7
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-11-14 11:58:12 +0100

    Add project files.

---
+++ frmMain.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Drawing;
+using A = DocumentFormat.OpenXml.Drawing;
+using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
+using DocumentFormat.OpenXml;
+using System.IO;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
+
+namespace Word_To_PDF
+{
+    public partial class frmMain : Form
+    {
+        private bool isDragging = false;
+        private int offsetX, offsetY;
+        // Create an OpenFileDialog for selecting a PDF file
+        OpenFileDialog pdfFileDialog = new OpenFileDialog
+        {
+            Filter = "PDF Files|*.pdf",
+            Title = "Select a PDF File"
+        };
+
+
+        public frmMain(int patientId = 0)
+        {
+            InitializeComponent();
+            Load += form_Load;
+            panel1.MouseDown += Panel1_MouseDown;
+            panel1.MouseMove += Panel1_MouseMove;
+            panel1.MouseUp += Panel1_MouseUp;
+            panel2.MouseDown += Panel1_MouseDown;
+            panel2.MouseMove += Panel1_MouseMove;
+            panel2.MouseUp += Panel1_MouseUp;
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+            if (pdfFileDialog.ShowDialog() == DialogResult.OK)
+                    lblFileName.Text = pdfFileDialog.FileName;
+        }
+
+        private void bbtnAddTreatment_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(pdfFileDialog.FileName))
+            {
+                MessageBox.Show("Select a file!"); return;
+            }
+            // Create a SaveFileDialog for choosing the Word document save location
+            SaveFileDialog wordSaveFileDialog = new SaveFileDialog
+            {
+                Filter = "Word Documents|*.docx",
+                Title = "Save Word Document"
+            };
+            if (wordSaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string wordSaveFilePath = wordSaveFileDialog.FileName;
+
+                // Now, you have the paths to the selected PDF file and Word document save location
+                // You can proceed to convert the PDF to Word and save it to the specified location
+                ConvertPdfToWord(pdfFileDialog.FileName, wordSaveFilePath);
+                MessageBox.Show("Completed!");
+            }
+        }
+
+        private void ConvertPdfToWord(string fileName, string wordSaveFilePath)
+        {
+            // Open the PDF file
+            using (PdfReader pdfReader = new PdfReader(fileName))
+            {
+                string pdfText = "";
+                for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+                {
+                    //ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    //pdfText += PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+                    ITextExtractionStrategy strategy = new LocationTextExtractionStrategy();
+                    pdfText += PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
+
+
+                    PdfDictionary pageDict = pdfReader.GetPageN(page);
+                    PdfDictionary resources = pageDict.GetAsDict(PdfName.RESOURCES);
+                    ExtractImagesFromPage(resources, pdfReader);
+                }
+
+                // Create a new Word document
+                using (var wordprocessingDocument =
+                    WordprocessingDocument.Create(wordSaveFilePath, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = wordprocessingDocument.AddMainDocumentPart();
+                    mainPart.Document = new DocumentFormat.OpenXml.Wordprocessing.Document();
+                    Body body = mainPart.Document.AppendChild(new Body());
+                    DocumentFormat.OpenXml.Wordprocessing.Paragraph para = body.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Paragraph());
+                    DocumentFormat.OpenXml.Wordprocessing.Run run = para.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Run());
+                    run.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Text(pdfText));
+                    wordprocessingDocument.MainDocumentPart.Document.Save();
+                }
+            }
+        }
+
+        // Function to extract images
+        void ExtractImagesFromPage(PdfDictionary resources, PdfReader pdfReader)
+        {
+            if (resources == null) return;
+
+            PdfDictionary xObjects = resources.GetAsDict(PdfName.XOBJECT);
+            if (xObjects == null) return;
+
+            foreach (PdfName name in xObjects.Keys)
+            {
+                PdfObject obj = xObjects.Get(name);
+                if (obj != null && obj.IsIndirect())
+                {
+                    PdfDictionary imgObject = (PdfDictionary)PdfReader.GetPdfObject(obj);
+
+                    if (imgObject != null && imgObject.Get(PdfName.SUBTYPE) != null && imgObject.Get(PdfName.SUBTYPE).Equals(PdfName.IMAGE))
+                    {
+                        int xrefIdx = Convert.ToInt32(((PRIndirectReference)obj).Number.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                        PdfObject pdfObj = pdfReader.GetPdfObject(xrefIdx);
+                        PdfStream pdfStrem = (PdfStream)pdfObj;
+                        byte[] bytes = PdfReader.GetStreamBytesRaw((PRStream)pdfStrem);
+
+                        // 'bytes' now contains the bytes of the image; you can save/process it as needed
+                        // Example: File.WriteAllBytes("outputImage.png", bytes);
+                    }
+                }
+            }
+        }
+
+        public static void InsertImage(MainDocumentPart mainPart, string imagePath)
+        {
+            ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+            //using (FileStream stream = new FileStream(imagePath, FileMode.Open))
+            //{
+            //    imagePart.FeedData(stream);
+            //}
+
+            string imageRelationshipId = mainPart.GetIdOfPart(imagePart);
+
+            // Define the image dimensions
+            long cx = 3000000L; // Width in EMUs
+            long cy = 2000000L; // Height in EMUs
+
+            // Create a drawing element with the image
+            Drawing drawing = new Drawing();
+            DW.Inline inline = new DW.Inline(
+                new DW.Extent() { Cx = cx, Cy = cy },
+                new DW.EffectExtent() { LeftEdge = 0L, TopEdge = 0L, RightEdge = 0L, BottomEdge = 0L },
+                new DW.DocProperties() { Id = (UInt32Value)1U, Name = "Picture 1" },
+                new DW.NonVisualGraphicFrameDrawingProperties(
+                    new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                new A.Graphic(
+                    new A.GraphicData(
+                        new PIC.Picture(
+                            new PIC.NonVisualPictureProperties(
+                                new PIC.NonVisualDrawingProperties() { Id = (UInt32Value)0U, Name = "New Bitmap Image.jpg" },
+                                new PIC.NonVisualPictureDrawingProperties()
+                            ),
+                            new PIC.BlipFill(
+                                new A.Blip() { Embed = imageRelationshipId, CompressionState = A.BlipCompressionValues.Print },
+                                new A.Stretch(new A.FillRectangle())
+                            )
+                        )
+                    )
+                )
+            );
+            inline.Append(drawing);
+
+            // Add the image to the paragraph
+            A.Paragraph imageParagraph = new A.Paragraph(new A.Run(inline));
+            mainPart.Document.Body.Append(imageParagraph);
+        }
+
+        public static void InsertImageOld(MainDocumentPart mainPart, string imagePath)
+        {
+            ImagePart imagePart = mainPart.AddImagePart(ImagePartType.Jpeg);
+            using (FileStream stream = new FileStream(imagePath, FileMode.Open))
+            {
+                imagePart.FeedData(stream);
+            }
+
+            string imageRelationshipId = mainPart.GetIdOfPart(imagePart);
+
+            // Define the image dimensions
+            long cx = 3000000L; // Width in EMUs
+            long cy = 2000000L; // Height in EMUs
+
+            // Create a drawing element with the image
+            Drawing drawing = new Drawing();
+            DW.Inline inline = new DW.Inline(
+                new DW.Extent() { Cx = cx, Cy = cy },
+                new DW.EffectExtent() { LeftEdge = 0L, TopEdge = 0L, RightEdge = 0L, BottomEdge = 0L },
+                new DW.DocProperties() { Id = (UInt32Value)1U, Name = "Picture 1" },
+                new DW.NonVisualGraphicFrameDrawingProperties(
+                    new A.GraphicFrameLocks() { NoChangeAspect = true }),
+                new A.Graphic(
+                    new A.GraphicData(
+                        new PIC.Picture(
+                            new PIC.NonVisualPictureProperties(
+                                new PIC.NonVisualDrawingProperties() { Id = (UInt32Value)0U, Name = "New Bitmap Image.jpg" },
+                                new PIC.NonVisualPictureDrawingProperties()),
+                            new PIC.BlipFill(
+                                new A.Blip(
+                                    new A.BlipExtensionList(
+                                        new A.BlipExtension() { Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" }),
+                                    new A.BlipExtension() { Uri = "{05A4C25C-085E-4A06-A0C0-9D07862D23EE}" })
+                                {
+                                    Embed = imageRelationshipId,
+                                    CompressionState = A.BlipCompressionValues.Print,
+                                },
+                                new A.Stretch(
+                                    new A.FillRectangle())),
+                            new PIC.ShapeProperties(
+                                new A.Transform2D(
+                                    new A.Offset() { X = 0L, Y = 0L },
+                                    new A.Extents() { Cx = cx, Cy = cy }),
+                                new A.PresetGeometry(
+                                    new A.AdjustValueList())
+                                { Preset = A.ShapeTypeValues.Rectangle }))
+                    //{ Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
+                    //{ Macro = "" }));
+            )));
+            inline.Append(drawing);
+
+            // Add the image to the paragraph
+            A.Paragraph imageParagraph = new A.Paragraph(new A.Run(inline));
+            mainPart.Document.Body.Append(imageParagraph);
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                offsetX = e.X;
+                offsetY = e.Y;
+            }
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                this.Left = e.X + this.Left - offsetX;
+                this.Top = e.Y + this.Top - offsetY;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+    }
+}
---
+++ frmMain.designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Word_To_PDF
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnAddTreatment = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::Word_To_PDF.Properties.Resources.i_card_bg_white;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.btnAddTreatment);
+            this.panel1.Controls.Add(this.lblFileName);
+            this.panel1.Location = new System.Drawing.Point(5, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(990, 507);
+            this.panel1.TabIndex = 55;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Transparent;
+            this.btnBrowse.BackgroundImage = global::Word_To_PDF.Properties.Resources.button_blue_outline;
+            this.btnBrowse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnBrowse.FlatAppearance.BorderSize = 0;
+            this.btnBrowse.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btnBrowse.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnBrowse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnBrowse.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnBrowse.Image = global::Word_To_PDF.Properties.Resources.camera_32;
+            this.btnBrowse.Location = new System.Drawing.Point(59, 174);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(144, 41);
+            this.btnBrowse.TabIndex = 53;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBrowse.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnAddTreatment
+            // 
+            this.btnAddTreatment.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.BackgroundImage = global::Word_To_PDF.Properties.Resources.button_blue;
+            this.btnAddTreatment.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddTreatment.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddTreatment.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnAddTreatment.FlatAppearance.BorderSize = 0;
+            this.btnAddTreatment.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnAddTreatment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddTreatment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAddTreatment.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAddTreatment.Location = new System.Drawing.Point(222, 176);
+            this.btnAddTreatment.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAddTreatment.Name = "btnAddTreatment";
+            this.btnAddTreatment.Size = new System.Drawing.Size(138, 41);
+            this.btnAddTreatment.TabIndex = 52;
+            this.btnAddTreatment.Text = "Process";
+            this.btnAddTreatment.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAddTreatment.UseVisualStyleBackColor = false;
+            this.btnAddTreatment.Click += new System.EventHandler(this.bbtnAddTreatment_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.lblFileName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(149)))), ((int)(((byte)(162)))));
+            this.lblFileName.Location = new System.Drawing.Point(53, 137);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(115, 25);
+            this.lblFileName.TabIndex = 47;
+            this.lblFileName.Text = "Select a file:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Image = global::Word_To_PDF.Properties.Resources.i_close;
+            this.btnClose.Location = new System.Drawing.Point(934, 10);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(42, 35);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnClose.TabIndex = 54;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::Word_To_PDF.Properties.Resources.i_card_bg_top;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(5, 4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(990, 50);
+            this.panel2.TabIndex = 55;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(78)))), ((int)(((byte)(100)))));
+            this.label1.Location = new System.Drawing.Point(37, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 30);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "PDF To Word";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Word_To_PDF.Properties.Resources.i_card_bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 520);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "frmMain";
+            this.Text = "frmPatientCard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnAddTreatment;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
